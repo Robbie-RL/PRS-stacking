4a151a7e8b804f4079bdd83636084a6c91a45a2f
Robbie Lualhati <robbie.lualhati@gmail.com>
2022-01-18 12:56:14 +1100

    Final base QC file and script

--- Base and target data QC.R
+++ Base and target data QC.R
@@ -1,9 +1,39 @@
 ### Bigsnpr stacking model ###
 
-# QC of base and target files
+### QC of base and target files ###
 
 
 # Base data : "Height.QC.gz"
-# Target data: "EUR"
+# Target data: "EUR.zip"
 
-info <- read.table("Height.QC.gz", header=T)
+setwd("/Users/robryan/Desktop/Masters project/Height stacking model")
+
+
+# Read in zipped base file 
+library(data.table)
+dat <- fread("Height.gwas.txt.gz") #529504 individuals, 11 columns
+
+
+# Create subset where INFO > 0.8 and MAF > 0.01
+subset <- dat[INFO > 0.8 & MAF > 0.01] 
+fwrite(subset, "Height.gz", sep="\t") #tab deliminated and zipped
+
+
+
+# Use plink to remove duplicate and ambiguous SNPs
+
+# To remove duplicate SNPs:
+#  gunzip -c Height.gz |\
+#  awk '{seen[$3]++; if(seen[$3]==1){ print}}' |\
+#  gzip - > Height.nodup.gz
+
+# To remove ambiguous SNPs
+#  gunzip -c Height.nodup.gz |\
+#   awk '!( ($4=="A" && $5=="T") || \
+#           ($4=="T" && $5=="A") || \
+#           ($4=="G" && $5=="C") || \
+#           ($4=="C" && $5=="G")) {print}' |\
+#   gzip > Height.QC.gz
+
+
+# Base data is now QC complete: Height.QC.gz
